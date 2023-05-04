929d0fc694082a3b0b3f18f0c70b11b9af75c658
Erik Orsolin <depaulaerik670@gmail.com>
2023-05-04 10:21:32 -0300

    correção de bug na chamada dos decoradores

--- pessoa.py
+++ pessoa.py
@@ -1,38 +1,38 @@
 class Pessoa:
-    def __init__(self, nome, cpf, telefone, cep):
-        self.nome = nome
-        self.cpf = cpf
-        self.telefone = telefone
-        self.cep = cep
+    def __init__(self, nome: str, cpf: str, telefone: str, cep: str):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__telefone = telefone
+        self.__cep = cep
 
     @property
     def nome(self):
-        return self.nome
+        return self.__nome
     
     @property
     def cpf(self):
-        return self.cpf
+        return self.__cpf
     
     @property
     def telefone(self):
-        return self.telefone
+        return self.__telefone
     
     @property
     def cep(self):
-        return self.cep
+        return self.__cep
     
     @nome.setter
     def nome(self, nome):
-        self.nome = nome
+        self.__nome = nome
     
     @cpf.setter
     def cpf(self, cpf):
-        self.cpf = cpf
+        self.__cpf = cpf
     
     @telefone.setter
     def telefone(self, telefone):
-        self.telefone = telefone
+        self.__telefone = telefone
     
     @cep.setter
     def cep(self, cep):
-        self.cep = cep
+        self.__cep = cep
