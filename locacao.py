1f47e32d5e91ca6223e5d63afcb642ec29231b4c
Erik Orsolin <depaulaerik670@gmail.com>
2023-05-04 19:05:40 -0300

    mudança em variáveis

--- locacao.py
+++ locacao.py
@@ -1,15 +1,18 @@
-class locacao:
+class Locacao:
     def __init__(self):
         self.n_dias = 0
         self.custo_locacao = 0
-        self.id_cliente = 0
+        self.cpf_cliente = None
         self.automovel = None
+        self.data_locacao = None
+        self.data_devolucao = None
 
-    def locar(self, id_cliente, automovel, n_dias):
-        self.id_cliente = id_cliente
+    def locar(self, cpf_cliente, automovel, n_dias):
+        self.cpf_cliente = cpf_cliente
         self.automovel = automovel
         self.n_dias = n_dias
         self.custo_locacao = n_dias * automovel.get_valor_diaria()
+        automovel.set_locado(True)
     
     def get_n_dias(self):
         return self.n_dias
@@ -23,16 +26,28 @@ class locacao:
     def set_custo_locacao(self, custo_locacao):
         self.custo_locacao = custo_locacao
 
-    def get_id_cliente(self):
-        return self.id_cliente
+    def get_cpf_cliente(self):
+        return self.cpf_cliente
     
-    def set_id_cliente(self, id_cliente):
-        self.id_cliente = id_cliente
+    def set_cpf_cliente(self, cpf_cliente):
+        self.cpf_cliente = cpf_cliente
 
     def get_automovel(self):
         return self.automovel
 
+    def set_automovel(self, automovel):
+        self.automovel = automovel
 
+    def get_data_locacao(self):
+        return self.data_locacao
+    
+    def set_data_locacao(self, data_locacao):
+        self.data_locacao = data_locacao
 
+    def get_data_devolucao(self):
+        return self.data_devolucao
+    
+    def set_data_devolucao(self, data_devolucao):
+        self.data_devolucao = data_devolucao
     
     
